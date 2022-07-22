f5d6171c1311f7788bc287a63a027f6f8f9827fa
zrierc <68382685+zrierc@users.noreply.github.com>
2022-07-22 15:40:20 +0700

    define s3 for storing static web and cloudfront for cdn

--- lib/syntheticode-stack.ts
+++ lib/syntheticode-stack.ts
@@ -1,10 +1,50 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
+import * as cdk from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as cloudFront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3Deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as path from 'path';
 import { Construct } from 'constructs';
 
 export class SyntheticodeStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // * Define Bucket
+    const webBucket = new s3.Bucket(this, 'webBucket', {
+      bucketName: 'static-web-cdk',
+      publicReadAccess: true,
+      websiteIndexDocument: 'index.html',
+      enforceSSL: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    // * Create distribution using cloud Front
+    const cdn = new cloudFront.Distribution(this, 'webDist', {
+      defaultBehavior: {
+        origin: new origins.S3Origin(webBucket),
+      },
+      defaultRootObject: 'index.html',
+    });
+
+    // * Deploy S3 as static site
+    new s3Deploy.BucketDeployment(this, 'DeployWebsite', {
+      sources: [s3Deploy.Source.asset(path.join(__dirname, '../src'))],
+      destinationBucket: webBucket,
+      distributionPaths: ['/*'],
+      distribution: cdn,
+      retainOnDelete: false,
+    });
+
+    // * Output the Endpoint
+    new cdk.CfnOutput(this, 'staticWebEndpoint', {
+      value: webBucket.bucketWebsiteUrl,
+    });
+    new cdk.CfnOutput(this, 'CDNDomainName', {
+      value: cdn.distributionDomainName,
+    });
   }
 }

d422792a1ef6598b0f6f29fdba97d82a523fbd7a
zrierc <68382685+zrierc@users.noreply.github.com>
2022-08-06 13:31:37 +0700

    create ecs construct

---
+++ lib/constructs/ecs-construct.ts
@@ -0,0 +1,59 @@
+import { Construct } from 'constructs';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import { StackProps } from 'aws-cdk-lib';
+import { Vpc } from 'aws-cdk-lib/aws-ec2';
+
+export interface EcsConstructProps extends StackProps {
+  /**
+   * Vpc that used for this cluster.
+   *
+   * @type {Vpc}
+   */
+  vpc: Vpc;
+}
+
+export class EcsConstruct extends Construct {
+  public taskDefinition: ecs.TaskDefinition;
+
+  constructor(scope: Construct, id: string, props: EcsConstructProps) {
+    super(scope, id);
+
+    // * Define Cluster
+    const cluster = new ecs.Cluster(this, 'test-cluster', {
+      vpc: props.vpc,
+    });
+
+    // * Define Serverless Task Definition
+    this.taskDefinition = new ecs.FargateTaskDefinition(
+      this,
+      'FargateTaskDef',
+      {
+        cpu: 512,
+        memoryLimitMiB: 1024,
+        runtimePlatform: {
+          operatingSystemFamily: ecs.OperatingSystemFamily.LINUX,
+        },
+      }
+    );
+
+    // * Attach container to task definition
+    this.taskDefinition.addContainer('webServer', {
+      image: ecs.ContainerImage.fromRegistry('xzrie/sample-php-ecs:1.0'),
+      portMappings: [
+        {
+          protocol: ecs.Protocol.TCP,
+          containerPort: 80,
+          hostPort: 8080, // ! Need to review
+        },
+      ],
+    });
+
+    // * Define serverless service
+    const service = new ecs.FargateService(this, 'test-service', {
+      cluster,
+      taskDefinition: this.taskDefinition,
+      desiredCount: 2,
+      circuitBreaker: { rollback: true },
+    });
+  }
+}

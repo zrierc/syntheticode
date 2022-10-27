b6a283b2c81e8d6d64a066c87ee48ce28129baab
zrierc <68382685+zrierc@users.noreply.github.com>
2022-10-27 16:38:46 +0700

    Initial commit

---
+++ test/ecs-workshop.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EcsWorkshop from '../lib/ecs-workshop-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ecs-workshop-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EcsWorkshop.EcsWorkshopStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});

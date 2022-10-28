b2d19b2906f7bd7153a5f7af6af01097b29d254a
zrierc <68382685+zrierc@users.noreply.github.com>
2022-10-28 17:02:46 +0700

    feat: define vpc construct

---
+++ lib/constructs/network-construct.ts
@@ -0,0 +1,29 @@
+import { Construct } from 'constructs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+
+export class VpcConstruct extends Construct {
+  public readonly vpc: ec2.Vpc;
+
+  constructor(scope: Construct, id: string) {
+    super(scope, id);
+
+    // * Define VPC
+    this.vpc = new ec2.Vpc(this, 'EcsWorkshopVPC', {
+      ipAddresses: ec2.IpAddresses.cidr('10.10.0.0/23'),
+      natGateways: 1,
+      maxAzs: 3,
+      subnetConfiguration: [
+        {
+          name: 'public',
+          subnetType: ec2.SubnetType.PUBLIC,
+          cidrMask: 26,
+        },
+        {
+          name: 'private',
+          subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+          cidrMask: 26,
+        },
+      ],
+    });
+  }
+}

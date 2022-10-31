32e279404ba1d8f01e1eb0d1bd62a4d2ca2e50a8
zrierc <68382685+zrierc@users.noreply.github.com>
2022-10-31 19:13:14 +0700

    feat: define core resources - vpc - ecs cluster - ecs service + task definition - application load balancer

--- lib/ecs-workshop-stack.ts
+++ lib/ecs-workshop-stack.ts
@@ -1,16 +1,36 @@
 import * as cdk from 'aws-cdk-lib';
+import * as ecs from 'aws-cdk-lib/aws-ecs';
+import { VpcConstruct } from './constructs/network-construct';
+import { EcsCluster } from './constructs/ecs-cluster-construct';
+import { EcsPattern } from './constructs/ecs-pattern-construct';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class EcsWorkshopStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // * Define VPC
+    const network = new VpcConstruct(this, 'VpcWorkshop');
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'EcsWorkshopQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // * Define Ecs Cluster
+    const workshopcluster = new EcsCluster(this, 'clusterWorkshop', {
+      vpc: network.vpc,
+      clusterName: 'cluster-workshop',
+    });
+
+    // * Define service + load balancer
+    const workshopService = new EcsPattern(this, 'workshopService', {
+      cluster: workshopcluster.ecsCluster,
+      vpc: network.vpc,
+      // See reference below to see taskImageOptions properties
+      // https://docs.aws.amazon.com/cdk/api/v2/docs/aws-cdk-lib.aws_ecs_patterns.ApplicationLoadBalancedTaskImageOptions.html
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromRegistry('lorem/ipsum'), // ! need to change to proper image
+        containerName: 'front-end_workshop',
+        family: 'front-end-services',
+      },
+    });
+
+    // ! need to add output e.g alb endpoint, cluster name, etc
   }
 }

7991183cfa1d09c7382e7cf7023743755500920f
zrierc <68382685+zrierc@users.noreply.github.com>
2022-11-08 15:21:07 +0700

    feat: add custom domain name for front-end service

--- lib/ecs-workshop-stack.ts
+++ lib/ecs-workshop-stack.ts
@@ -1,10 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import * as ecs from 'aws-cdk-lib/aws-ecs';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 import { VpcConstruct } from './constructs/network-construct';
 import { EcsCluster } from './constructs/ecs-cluster-construct';
 import { EcsPattern } from './constructs/ecs-pattern-construct';
 import { Construct } from 'constructs';
 import { Duration } from 'aws-cdk-lib';
+import { SslPolicy } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 
 export class EcsWorkshopStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -19,8 +22,18 @@ export class EcsWorkshopStack extends cdk.Stack {
       clusterName: 'cluster-workshop',
     });
 
-    // * Define front-end service + load balancer
-    const workshopService = new EcsPattern(this, 'workshopService', {
+    // * Get Domain Zone
+    const domainZone = route53.HostedZone.fromLookup(this, 'Zone', {
+      domainName: 'zrierc.systems',
+    });
+    const certificate = acm.Certificate.fromCertificateArn(
+      this,
+      'Cert',
+      'arn:aws:acm:us-east-1:838928449797:certificate/a26e792c-41e4-4375-b4ec-70c5b3fd22b6'
+    );
+
+    // * Define front-end service, load balancer, scaling policy
+    const frontEndService = new EcsPattern(this, 'frontEndService', {
       cluster: workshopcluster.ecsCluster,
       serviceName: 'fe-service',
       // See reference below to see taskImageOptions properties
@@ -37,11 +50,14 @@ export class EcsWorkshopStack extends cdk.Stack {
       taskSubnets: {
         subnetGroupName: 'private',
       },
+      domainName: 'web.zrierc.systems',
+      certificate,
+      domainZone,
+      sslPolicy: SslPolicy.RECOMMENDED_TLS,
     });
 
-    // * Define front-end service scaling policy
     const scalableFrontEndTarget =
-      workshopService.workshopService.service.autoScaleTaskCount({
+      frontEndService.workshopService.service.autoScaleTaskCount({
         minCapacity: 1,
         maxCapacity: 5,
       });
@@ -55,13 +71,12 @@ export class EcsWorkshopStack extends cdk.Stack {
     // * Tagging resources
     cdk.Tags.of(network).add('Author', 'anak-magang');
     cdk.Tags.of(workshopcluster).add('Author', 'anak-magang');
-    cdk.Tags.of(workshopService).add('Author', 'anak-magang');
+    cdk.Tags.of(frontEndService).add('Author', 'anak-magang');
 
     // * Output resources
-    new cdk.CfnOutput(this, 'ALB Endpoint', {
-      value: workshopService.workshopService.loadBalancer.loadBalancerDnsName,
+    new cdk.CfnOutput(this, 'ALB Front-End Endpoint', {
+      value: frontEndService.workshopService.loadBalancer.loadBalancerDnsName,
     });
-
     /*
     TODO: Here's todo list that need to be done in 2 days
     - add up to 5 container but running only 2 fargate
